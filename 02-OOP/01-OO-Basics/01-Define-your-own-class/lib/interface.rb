777f10c5ebfbe9ebf7f7076bd5efb0d626f15a23
Alexandro Nussbaumer <alexandronussbaumer@Alexandros-MacBook-Pro.local>
2021-12-20 18:47:10 +0100

    Restaurant

---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/interface.rb
@@ -0,0 +1,2 @@
+require_relative "restaurant"
+def new; end
---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/restaurant.rb
@@ -0,0 +1,13 @@
+# attr_reader name, type, location
+
+class Restaurant
+  def initialize(name, type, location)
+    @name = name
+    @type = type
+    @location = location
+  end
+
+  def show
+    @restaurant = restaurant
+  end
+end
