8ef87b420b064a7e8c93690ce8409c152692aff0
AlexandroKeith <alexandro.nussbaumer@gmail.com>
2021-12-23 17:06:37 +0100

    initiazlize method

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,14 @@
 class OrangeTree
   # TODO: Implement all the specs defined in the README.md :)
+  def initialize 
+    @age = 0
+    @height = 0 
+    @fruits = 0
+    @dead = false 
+  end
+
+  def dead? 
+    @dead 
+  end
+
 end
