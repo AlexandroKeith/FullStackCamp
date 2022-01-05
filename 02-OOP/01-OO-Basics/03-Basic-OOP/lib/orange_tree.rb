7f3b0e59aa234fdb830a93c8c0a7fc561331b93a
AlexandroKeith <alexandro.nussbaumer@gmail.com>
2022-01-05 08:27:51 +0100

    picked up my orange this morning

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,14 +1,50 @@
 class OrangeTree
+  attr_reader :age, :height, :fruits
+
   # TODO: Implement all the specs defined in the README.md :)
-  def initialize 
+  def initialize
     @age = 0
-    @height = 0 
+    @height = 0
     @fruits = 0
-    @dead = false 
+    @dead = false
   end
 
-  def dead? 
-    @dead 
+  def dead?
+    @dead
   end
 
+  def one_year_passes!
+    unless dead?
+      may_die!
+      grow!
+      produce_fruits!
+    end
+  end
+
+  def grow!
+    @age += 1
+    @height += 1 if @age <= 10
+  end
+
+  def may_die!
+    if @age == 99
+      @dead = true
+    elsif @age > 50
+      @dead = (@age >= rand(51..100))
+    end
+  end
+
+  def produce_fruits!
+    if @age > 5 && @age < 10
+      @fruits = 100
+    elsif @age >= 10 && @age < 15
+      @fruits = 200
+    else
+      @fruits = 0
+    end
+  end
+
+  def pick_a_fruit!
+    @fruits -= 1 if fruits.positive?
+  end
 end
